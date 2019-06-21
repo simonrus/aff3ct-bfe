af781ae26d322cddee197dd282acfea386b37e80
Sergei Semenov <sergey.semionov@gmail.com>
2019-06-21 05:46:41 +0200

    [2] Restructured

--- aff3ct-client/Aff3ctProtocol.py
+++ aff3ct-client/Aff3ctProtocol.py
@@ -13,12 +13,19 @@ class Aff3ctProtocol:
         matrix.values.extend([1.0, 2.0, 3.0, 4.0])
         return matrix
 
-
-    def do_push(self, var):
+    @staticmethod
+    def do_push(socket, var, matrix):
         request = aff3ct_pb2.PushRequest(mtx=Aff3ctProtocol.serialize_matrix(None))
         request.var = var
 
         message = aff3ct_pb2.Message(pushRequest=request)
         stream = message.SerializeToString()
+        print('message send')
+        socket.send(stream)
+
+        reply = socket.recv()
+        print('reply received')
+        data = reply.split(b' ', 1)[1]              # WTF?
+        message.ParseFromString(data)
 
         return True, ''

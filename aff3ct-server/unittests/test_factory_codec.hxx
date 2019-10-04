e63f02983fa1e9845c939d5fad8fca88ce4f6a55
Semenov Sergej (EXT) <sese4914@eso9504.eso.local>
2019-10-04 15:15:04 +0200

    OnlyCodec: Refactoring (p.7.5.1) Unittest started

---
+++ aff3ct-server/unittests/test_factory_codec.hxx
@@ -0,0 +1,30 @@
+/*****************************************************************************/
+/*
+    Copyright esolutions GmbH
+    All rights reserved
+    Authors:
+    Sergej Semenov (EXT)
+ * realize an optional as defined in 
+ *   [SWS_CM_01033][
+ */
+/*****************************************************************************/
+
+#ifndef TEST_FACTORY_CODEC_HXX
+#define TEST_FACTORY_CODEC_HXX
+
+#include <cxxtest/TestSuite.h>
+
+/*****************************************************************************/
+class Test_Factory_Codec : public CxxTest::TestSuite
+    {
+    public:
+        void testMethod( void )
+        {
+            TS_ASSERT( 1 + 1 > 1 );
+            TS_ASSERT_EQUALS( 1 + 1, 2 );
+        }
+    };
+
+/*****************************************************************************/
+#endif /* TEST_FACTORY_CODEC_HXX */
+

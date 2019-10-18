f9cd692075c93e1fc81021c3c107e2ba59b5385d
Semenov Sergej (EXT) <sese4914@eso9504.eso.local>
2019-10-18 20:08:01 +0200

    Started identity matrix build(swap??)

--- tools/LDPC/MatrixHandler.py
+++ tools/LDPC/MatrixHandler.py
@@ -1,8 +1,30 @@
 import numpy as np
 
 class MatrixHandler:
+
     def transform_H_to_G_indentity(self, H, info_bits_position):
-        pass
+        n_rows = H.shape[0]
+        n_cols = H.shape[1]
+        ## stage first move to triangle way
+        dim = min(n_rows,n_cols)
+        for i in range(0,dim):
+            print("next column %s" %(i))
+            anchor = None
+            anchor_row = 0
+            for j in range(i, dim):
+                if H[j,i] == 1:
+                    #swap row with i
+                    if anchor is None:
+                        anchor = H[j,:].copy()
+                        anchor_row = j
+                        H[j,:] = H[i,:]
+                        H[i,:] = anchor
+                    else:
+                        #substract
+                        H[j,:] = anchor ^ H[anchor_row,:]
+                    #substrace curent row from
+    
+        return H
 
 
     def transform_H_to_G_decomp_LU(self, H, info_bits_position):
--- tools/LDPC/tests/test_matrix_handler.py
+++ tools/LDPC/tests/test_matrix_handler.py
@@ -5,9 +5,10 @@ from MatrixHandler import *
 class MatrixHandlerTest(unittest.TestCase):
     def test_transform_H_to_G_identity(self):
         
-        matrix_H = np.matrix([  [0,0,1,1,1,0,1],
+        matrix_H = np.array([   [0,0,1,1,1,0,1],
                                 [0,1,1,1,0,1,0],
                                 [1,1,1,1,1,1,1]])
         handler = MatrixHandler()                         
         G = handler.transform_H_to_G_indentity(matrix_H, None)
+        print (G)
         pass

5de3e401b705c42dd0cf72adde7b6d2d753cf46c
Me on t420 <sergey.semionov@gmail.com>
2019-11-28 21:49:38 +0100

    prob. decoder, doesn't make sense to continue  # Please enter the commit message for your changes. Lines starting  # with '#' will be ignored, and an empty message aborts the commit.  # Explicit paths specified without -i or -o; assuming --only paths...  # On branch master  # Your branch is up-to-date with 'origin/master'.  #  # Changes to be committed:  #	modified:   LDPC/DecoderLDPCProb.py  #  # Changes not staged for commit:  #	modified:   .idea/workspace.xml  #

--- tools/LDPC/DecoderLDPCProb.py
+++ tools/LDPC/DecoderLDPCProb.py
@@ -1,3 +1,5 @@
+from os import waitid_result
+
 import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse import csc_matrix
@@ -40,36 +42,68 @@ class DecoderLDPCProb:
         likelihoods = np.exp(2 * received / self.sigma / self.sigma)
         prob_ones = likelihoods / (likelihoods + 1)
 
-        prob_ones = np.array(range(0,received.shape[0]))
-        prob_ones = prob_ones / sum(prob_ones)
+        #Test code
+        #prob_ones = np.array(range(0,received.shape[0]))
+        #prob_ones = prob_ones / sum(prob_ones)
+
         pa_prob_ones = prob_ones.copy()
         pa_mtx = np.zeros(self.H.shape)
 
-
+        pdb.set_trace()
         print("prob_ones ", prob_ones)
 
         for iter in range(0,self.max_num_iter):
             #horizontal step
 
-
+            print("HORIZ step input")
+            print(pa_prob_ones)
             for row_index in range(0, self.N - self.K):
                 row = self.csrow_H.getrow(row_index)
                 weight = row.sum()
 
                 pos_ones = np.where(row.toarray().flatten() == 1)
-                pa_prob_ones = 1 - 2 * pa_prob_ones
-                pdb.set_trace()
-                prod = np.prod(pa_prob_ones[pos_ones])
 
-                pa_mtx[row_index, pos_ones] = np.ones(weight) * prod / pa_prob_ones[pos_ones]
+
+                ##
+                #
+                #                   1 + П (1 - 2 * P_t)
+                # P(sum=odd) =      -------------------
+                #                           2
+                ##<
+
+                temp = 1 - 2 * pa_prob_ones
+                prod = np.prod(temp[pos_ones])
+                pa_mtx[row_index, pos_ones] = np.ones(weight) * prod / temp[pos_ones]
                 pa_mtx[row_index, pos_ones] = (pa_mtx[row_index, pos_ones] + 1)/2
 
                 #shall be ok
 
+            print("HORIZ step output")
+            print(pa_mtx)
+
+            for col_index in range(0, self.N):
+                col = self.cscol_H.getcol(col_index)
+                weight = col.sum()
+
+                #pdb.set_trace()
+                pos_ones = np.where(col.toarray().flatten() == 1)
+
+
+                ##
+                #
+                #                   P(C) * П(Xn | c)
+                # P(C|X1 .. Xn) =   -------------------
+                #                      P(X1..Xn)
+                ##<
+
+                temp = pa_mtx[pos_ones, col_index]
+                prod = np.prod(temp)
+                value = pa_prob_ones[col_index] * (1 << weight) * prod
+
+                pa_prob_ones[col_index] = value #assign new value
 
         print("DONE")
-        print(pa_mtx)
-        print("DONE")
+        print(pa_prob_ones)
 
 
 

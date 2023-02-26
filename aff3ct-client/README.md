1c1dbe98ecaee80ac09916fe9cac4c71157fd6a1
Sergei Semenov <sergey.semionov@gmail.com>
2023-02-26 13:09:54 +0100

    Updated description

--- aff3ct-client/README.md
+++ aff3ct-client/README.md
@@ -1,3 +1,7 @@
+source ~/bin/anaconda.sh
+conda activate py3
+
 # Run Unittest
 python -m unittest test_Aff3ctProtocol.py
 # Run sample application
+python aff3ct-client.py
--- aff3ct-server/prepare.sh
+++ aff3ct-server/prepare.sh
@@ -2,6 +2,6 @@ mkdir cmake && mkdir cmake/Modules
 cp ../lib/aff3ct/build/lib/cmake/aff3ct-*/* cmake/Modules
 mkdir build
 cd build
-cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-funroll-loops -march=native"
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-march=native -ggdb"
 make
 
